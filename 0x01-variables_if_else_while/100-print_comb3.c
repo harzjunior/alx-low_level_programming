7cffa563e31801efda661ec4fb0f1374f32751aa
harzjunior <harunjibs@gmail.com>
2023-02-19 03:10:08 -0800

    g/0x01-variables_if_else_while

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,22 +11,21 @@
 int main(void)
 {
 int n1 = 48;
-int n2 = 48;
-int com = 44;
+int n2;
 
-while (n1 <= 57)
+while (n1 <= 56)
 {
-n2 = 48;
+n2 = n1 + 1;
 
 while (n2 <= 57)
 {
 putchar(n1);
 putchar(n2);
 
-if (n1 != 57 || n2 != 57)
+if (n1 != 56 || n2 != 57)
 {
-putchar(com);
-putchar(32);
+putchar(',');
+putchar(' ');
 }
 
 n2 += 1;
