c64c30c24c08b2c94088cb9cbff15f22352e27dc
harzjunior <harunjibs@gmail.com>
2023-02-19 02:59:40 -0800

    g/0x01-variables_if_else_while

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+nclude <stdio.h>
 
 /**
  *main - program that prints the numbers from 00 to 99
@@ -10,27 +10,32 @@
 
 int main(void)
 {
-	int n1 = 48;
-	int n2 = 48;
-        int com = 44;
+int n1 = 48;
+int n2 = 48;
+int com = 44;
 
-	while (n1 <= 57)
-	{
-        n2 = 48;
-        while(n2 <= 57)
-	{
-	putchar(n1);
-	putchar(n2);
+while (n1 <= 57)
+{
+n2 = 48;
 
-	if (n1 != 57 || n2 != 57)
-	{
-	putchar(com);
-	putchar(32);
-	}
-	n2 += 1;
-	}
-	n1 += 1;
-	}
-	putchar('\n');
-	return (0);
+while (n2 <= 57)
+{
+putchar(n1);
+putchar(n2);
+
+if (n1 != 57 || n2 != 57)
+{
+putchar(com);
+putchar(32);
+}
+
+n2 += 1;
+
+}
+n1 += 1;
+}
+putchar('\n');
+
+return (0);
+
 }
