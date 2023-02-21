3e74c5265678f8037556cb8d33a96616a3171477
harzjunior <harunjibs@gmail.com>
2023-02-21 09:19:24 -0800

    /0x02-functions_nested_loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * main - program that prints Holberton.
@@ -9,7 +9,7 @@
 int main(void)
 {
 	int n = 0;
-	char str_hbt[9] = "Holberton";
+	char str_hbt[30] = "abcdefghijklmnopqrstuvwxyz";
 
 	while (n < 9)
 	{
