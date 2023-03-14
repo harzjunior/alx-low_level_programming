1544fe466be14f1331b630310375d3b2072bb3ed
harzjunior <harunjibs@gmail.com>
2023-03-14 05:03:07 -0700

    /0x0B-malloc_free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,12 +14,12 @@ char *arr = NULL;
 unsigned int i;
 
 if (size == 0)
-return NULL;
+return (NULL);
 arr = malloc(size * sizeof(char));
 if (arr != NULL)
 {
 for (i = 0; i < size; i++)
 arr[i] = c;
 }
-return arr;
+return (arr);
 }
