07ac5ccf1443935dc678ec4d0e4460ec4db2d956
harzjunior <harunjibs@gmail.com>
2023-03-21 01:11:03 -0700

    0x0D-preprocessor

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,10 +1,8 @@
-#ifndef MACRO_H 
- #define MACRO_H 
- /** 
-  *   * File: 1-pi 
-  *    * 
-  *     * Desc: header file 
-  *      */ 
- #define PI 3.14159265359 
-  
- #endif
+#ifndef MACRO_H
+#define MACRO_H
+/**
+* File: 1-pi
+* Desc: header file
+*/
+#define PI 3.14159265359
+#endif
--- 0x0D-preprocessor/main.h
+++ 0x0D-preprocessor/main.h
@@ -1,9 +1,10 @@
-#ifndef MAIN_H 
-#define MAIN_H 
-/** 
-*File: main  
-*Desc: header file 
-*      */ 
-int _putchar(char c); 
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+*File: main
+*Desc: header file
+*/
+int _putchar(char c);
 
 #endif
