99343e5605545efba864de3c1c19fcc981586ccc
harzjunior <harunjibs@gmail.com>
2023-04-11 12:03:06 -0700

    0x14-bit_manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,10 +4,8 @@
  * binary_to_uint - converts a binary number to an
  * unsigned int.
  * @b: binary.
- *
  * Return: unsigned int.
  */
-
 unsigned int binary_to_uint(const char *b)
 {
 unsigned int ui;
--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -4,14 +4,12 @@
 /**
  * print_binary - prints the binary representation of a number
  * @n: unsigned long int to be converted
- *
  * Return: void
  */
 void print_binary(unsigned long int n)
 {
-    if (n > 1)
-        print_binary(n >> 1);
+if (n > 1)
+print_binary(n >> 1);
 
-    putchar((n & 1) + '0');
+putchar((n & 1) + '0');
 }
-
